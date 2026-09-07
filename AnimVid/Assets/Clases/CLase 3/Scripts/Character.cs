019c7298cd92c24218fff6f431402e4401fbeefc
nijesa <164059820+nijesa@users.noreply.github.com>
2026-09-07 16:37:23 -0500

    3 personajes jugables listos

--- AnimVid/Assets/Clases/CLase 3/Scripts/Character.cs
+++ AnimVid/Assets/Clases/CLase 3/Scripts/Character.cs
@@ -3,30 +3,50 @@ using UnityEngine;
 [DefaultExecutionOrder(-1)]
 public class Character:MonoBehaviour
 {
-    [SerializeField] private bool isAiming;
-    private Transform lockTarget;
-    public bool IsAiming
-    {
-        get=>isAiming;
-        set=> isAiming = value;
-    }
-
-    public Transform LockTarget
-    {
-        get => lockTarget;
-        set => lockTarget = value;
-    }
-    private void Awake()
-    {
-        RegisterComponents();
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    private void RegisterComponents()
-    {
-        foreach (ICharacterComponent characterComponent in GetComponentsInChildren<ICharacterComponent>())
+    private bool isCrouching;
+        public bool IsCrouching
         {
-            characterComponent.ParentCharacter = this;
+            get => isCrouching;
+            set => isCrouching = value;
         }
-    }
+        private bool isReloading;
+        public bool IsReloading
+        {
+            get => isReloading;
+            set => isReloading = value;
+        }
+        private bool isWeaponEquipped = true;
+        public bool IsWeaponEquipped
+        {
+            get => isWeaponEquipped;
+            set => isWeaponEquipped = value;
+        }
+        private Transform lockTarget;
+        private bool isAiming;
+        public bool IsAiming
+        {
+            get => isAiming;
+            set => isAiming = value;
+        }
+        
+
+        public Transform LockTarget
+        {
+            get =>lockTarget;
+            set =>lockTarget = value;
+        }
+        
+        private void Awake()
+        {
+            RegisterComponents();
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void RegisterComponents()
+        {
+            foreach (ICharacterComponent component in GetComponentsInChildren<ICharacterComponent>())
+            {
+                component.ParentCharacter = this;
+            }
+        }
 }
--- AnimVid/Assets/Clases/CLase 3/Scripts/CharacterMovement.cs
+++ AnimVid/Assets/Clases/CLase 3/Scripts/CharacterMovement.cs
@@ -3,71 +3,77 @@ using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour, ICharacterComponent
 {
-    public Character ParentCharacter { get; set; }
-
-    [SerializeField] private FloatDamper speedX;
-    [SerializeField] private FloatDamper speedY;
-    [SerializeField] private float angularSpeed;
-
-    [SerializeField] private Camera _camera;
-    private Animator _animator;
-    private int _speedXHash;
-    private int _speedYHash;
-
-    private Quaternion targetRotation;
-
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-        _speedXHash = Animator.StringToHash("SpeedX");
-        _speedYHash = Animator.StringToHash("SpeedY");
-    }
-
-    public void OnMove(InputAction.CallbackContext ctx)
-    {
-        Vector2 inputValue = ctx.ReadValue<Vector2>();
-
-        speedX.targetValue = inputValue.x;
-        speedY.targetValue = inputValue.y;
-
+     [SerializeField] private FloatDamper speedX;
+        [SerializeField] private FloatDamper speedY;
+        [SerializeField] private Camera camera;
+        [SerializeField] private float angularSpeed;
+        private Quaternion targetRotation;
         
-    }
+        private int _speedXHash;
+        private int _speedYHash;
+        private Animator _animator;
 
-    private void SolveCharacterRotation()
-    {
-        Vector3 floorNormal = transform.up;
-        Vector3 cameraRealForward = _camera.transform.forward;
-
-        float angleInterpolator = Mathf.Abs(Vector3.Dot(cameraRealForward, floorNormal));
-        Vector3 cameraForward = Vector3.Lerp(cameraRealForward, _camera.transform.up, angleInterpolator).normalized;
-        Vector3 characterForward = Vector3.ProjectOnPlane(cameraForward, floorNormal).normalized;
-
-        Debug.DrawLine(transform.position, transform.position + characterForward * 2, Color.magenta,5);
-
-        targetRotation = Quaternion.LookRotation(characterForward, floorNormal);
-    }
-
-    private void ApplyCharacterRotation()
-    {
-        float motionMagnitude = Mathf.Sqrt(speedX.targetValue*speedX.targetValue + speedY.targetValue*speedY.targetValue);
-        float rotationSpeed = Mathf.SmoothStep(0,.1f, motionMagnitude);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, angularSpeed * rotationSpeed );
-    
-
-    }
-
-    private void Update()
-    {
-       speedX.Update();
-       speedY.Update();
-
-       _animator.SetFloat(_speedXHash, speedX.currentValue);
-        _animator.SetFloat(_speedYHash, speedY.currentValue);
-
-        SolveCharacterRotation();
-        if(!ParentCharacter.IsAiming)
+        private void Awake()
         {
-            ApplyCharacterRotation();
+            _animator = GetComponent<Animator>();
+            _speedXHash = Animator.StringToHash("SpeedX");
+            _speedYHash = Animator.StringToHash("SpeedY");
         }
-    }
+
+        private void SolveCharacterRotation()
+        {
+            Vector3 floorNormal = transform.up;
+            Vector3 cameraRealForward = camera.transform.forward;
+            float angleInterpolator = Mathf.Abs(Vector3.Dot(cameraRealForward, floorNormal));
+            Vector3 cameraForward = Vector3.Lerp(cameraRealForward, camera.transform.up, angleInterpolator).normalized;
+            Vector3 characterForward = Vector3.ProjectOnPlane(cameraForward,floorNormal).normalized;
+            Debug.DrawLine(transform.position, transform.position + characterForward*3, Color.green,5);
+            targetRotation = Quaternion.LookRotation(characterForward,floorNormal);
+        }
+
+        [SerializeField] private float crouchSpeedMultiplier = 0.5f;
+        private Vector2 _rawInput;
+
+        public void OnMove(InputAction.CallbackContext ctx)
+        {
+            _rawInput = ctx.ReadValue<Vector2>();
+        }
+        private void Update()
+        {
+            float multiplier = (ParentCharacter != null && ParentCharacter.IsCrouching) ? crouchSpeedMultiplier : 1f;
+            speedX.targetValue = _rawInput.x * multiplier;
+            speedY.targetValue = _rawInput.y * multiplier;
+
+            speedX.Update();
+            speedY.Update();
+            _animator.SetFloat(_speedXHash,speedX.currentValue);
+            _animator.SetFloat(_speedYHash,speedY.currentValue);
+            SolveCharacterRotation();
+            if (ParentCharacter != null && ParentCharacter.IsAiming)
+            {
+                // Al apuntar, alinear suavemente el cuerpo con la cámara para que la cadera no se deforme
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, angularSpeed * 8f * Time.deltaTime);
+            }
+            else
+            {
+                ApplyCharacterRotation();
+            }
+        }
+
+        private void ApplyCharacterRotation()
+        {
+            float motionMagnitud = Mathf.Sqrt(speedX.targetValue * speedX.targetValue + speedY.targetValue * speedY.targetValue);
+            float rotationSpeed = Mathf.SmoothStep(0, .01f, motionMagnitud);
+
+            // Si está quieto pero el ángulo con la cámara supera los 70 grados, girar el cuerpo para no torcer la cintura
+            float angleToCamera = Quaternion.Angle(transform.rotation, targetRotation);
+            if (motionMagnitud < 0.01f && angleToCamera > 70f)
+            {
+                rotationSpeed = Mathf.Clamp01((angleToCamera - 70f) / 30f);
+            }
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, angularSpeed * rotationSpeed);
+        }
+
+        public Character ParentCharacter { get; set; }
 }
